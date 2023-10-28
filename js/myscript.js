e3660d058c9e94ad45443a15dd998ad208fc82c2
hisham3amer <111225608+hisham3amer@users.noreply.github.com>
2023-10-28 23:45:19 +0200

    5

---
+++ js/myscript.js
@@ -0,0 +1,75 @@
+let searchIcon = document.getElementById("searchIcon");
+let searchWebsite = document.getElementById("searchWebsite");
+let searchToggle = document.getElementById("searchToggle");
+let dimming = document.getElementById("dimming");
+
+function searchOpen() {
+  if (searchIcon.className == "fa-solid fa-magnifying-glass icon") {
+    searchIcon.className = "fa-solid fa-xmark-large icon";
+    searchWebsite.style = "z-index: 31;";
+    searchToggle.style = "background-color: white;";
+    dimming.className = "covering-panel";
+  } else if (searchIcon.className == "fa-solid fa-xmark-large icon") {
+    searchIcon.className = "fa-solid fa-magnifying-glass icon";
+    searchWebsite.style = "z-index: -1;";
+    searchToggle.style = "background-color: transparent;";
+    dimming.className = "";
+  }
+}
+searchToggle.onclick = searchOpen;
+
+let websiteSearch = document.getElementById("websiteSearch");
+let courseSearch = document.getElementById("courseSearch");
+let searchData = document.getElementById("searchData");
+let searchRight = document.getElementById("searchRight");
+function courseActive() {
+  websiteSearch.className = "";
+  courseSearch.className = "active";
+  searchData.placeholder = "Search for a course";
+  searchRight.style =
+    "transform: translateX(0); opacity:100%; transition:1s; display:block !important;";
+}
+courseSearch.onclick = courseActive;
+
+function websiteActive() {
+  websiteSearch.className = "active";
+  courseSearch.className = "";
+  searchData.placeholder = "Search NTI Website";
+  searchRight.style =
+    "transform: translateX(100%); opacity:0; transition:1s; display:none;";
+}
+websiteSearch.onclick = websiteActive;
+
+// let navItem = document.querySelectorAll('.dropdown-menu');
+// let navLink = document.querySelectorAll('.nav-link');
+
+// function showDropDown(){
+//     navItem.style ='display:block;'
+// }
+// navLink.onclick = showDropDown;
+
+
+window.onscroll = function (e) {
+var vertical_position = 0;
+if (scrollY){
+    //usual
+    vertical_position = scrollY;}
+else if (document.documentElement.clientHeight){
+    //ie
+    vertical_position = document.documentElement.scrollTop;}
+else if (document.body){
+    //ie quirks
+    vertical_position = document.body.scrollTop;}
+    // console.log(scrollY);
+var nti = document.getElementById("nti");
+if (scrollY<140 && window.matchMedia('screen and (min-width: 767px)').matches){
+    console.log(screen.width);
+    nti.style.bottom = vertical_position + 0 + "px" ; //200 is arbitrary.. just to show you could now position it how you want
+}else if (window.matchMedia('screen and (max-width: 767px) and (min-width:575px)').matches && scrollY<90 ){
+    nti.style.bottom = vertical_position + 0 + "px" ;
+} else if (window.matchMedia('screen and (max-width: 576px)').matches && scrollY<65){
+    nti.style.bottom = vertical_position + 0 + "px" ;
+}
+};
+
+
